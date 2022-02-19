1ed1ac0152d080beba455e135f2c61d79cee81b8
DegKir <degtiarev.kd@phystech.edu>
2022-02-19 20:32:12 +0300

    Conjuncts and predicat logic added

--- Formulas.cpp
+++ Formulas.cpp
@@ -1,4 +1,5 @@
 #include<vector>
+#include<tuple>
 #include"Formulas.hpp"
 #include<iostream>
 // git init
@@ -9,7 +10,24 @@
 // git remote add origin <url>
 // git push
 // git git git giiiiiiiit
+
+
+
 Polynom::Polynom(std::vector<double> coefficients): _coefficients(coefficients){}
+
+std::vector<double> Polynom::get_coefficients()
+{
+	return _coefficients;
+}
+
+void Polynom::print()
+{
+	int size=_coefficients.size();
+	for (int i = 0; i < size-1;i++)
+		std::cout<<_coefficients[i]<<"x^"<<i<<"+";
+	std::cout<<_coefficients[size-1]<<"x^"<<size-1<<std::endl;
+}
+
 double Polynom::get_value(double point_x)
 {
 	double sum=0;
@@ -22,12 +40,149 @@ double Polynom::get_value(double point_x)
 	return sum;
 }	
 //not actually givint derivative
-void Polynom::get_derivative()
+Polynom Polynom::get_derivative()
 {
 //	std::cout<<"kek"<<_coefficients.size()<<"kek"<<std::endl;
 	std::vector<double> derivative_coefficients;
-	for (int i =0; i<_coefficients.size(); i++)
-		std::cout<<_coefficients[i]*i<<std::endl;
-//		derivative_coefficients.push_back(_coefficients[i]*i);
-//	Polynom D(derivative_coefficients);
+	for (int i =1; i<_coefficients.size(); i++)
+	{
+//		std::cout<<_coefficients[i]*i<<std::endl;
+		derivative_coefficients.push_back(_coefficients[i]*i);
+	}
+	return Polynom(derivative_coefficients);
 }
+
+
+int Polynom::get_degree()
+{
+	return _coefficients.size()-1;
+}
+
+std::pair<Polynom,Polynom> Polynom::divide(Polynom Pdivident,Polynom Pdivider)
+{
+//	std::vector<double> divident = Pdivident.get_coefficients();
+//	std::vector<double> divider = Pdivider.get_coefficients();
+//	std::vector<double> An=divident;
+//	std::vector<double> Qr();
+//check if degree of divider is bigger then degree of divident
+//	if (Pdivider.get_degree() > Pdivident.get_degree())
+//		std::cout<<"degree of divider is bigger then degree of divident"<<std::endl;
+//also should check what if divident.size=divider.size and maybe we should look on coefficients 
+//need to get
+//	while (An.size()=>divider.size())
+//	{
+//		if (An.size()==divider.size())
+//			if(An.back()<divier.back())
+//				break;
+//		if (An.first()==0)
+//		{
+//			Qr.pushback(0);
+//			continue;
+//		}
+//		//dont sure about krayniy cases
+//		std::vector<double> haha(An.size(),0);
+//		std::vector<double> razn();
+//		double q = An.back()/divider.back;
+//		Qr.pushback(q);
+//		//recheck this thins with some bright head
+//		for(int i=0;i<An.size();i++)
+//		{
+//			// kraynie points problem mabe
+//			if (i>=An.size()-divider.size())
+//				haha[i]=divider[i]*q;
+//			if (i<An.size()-divider.size())
+//				haha[i]=0;
+//		}
+//		//anti-plus
+//		for(int i =0; i<An.size();i++)
+//			razn.pushback(An[i]-haha[i]);
+//		An=razn;
+//	}
+//	std::pair<Polynom, Polynom> Result(Polynom(An),Polynom(Qr));
+//	return result;
+}
+
+Predicate::Predicate(double constant, double point, Polynom polynom, bool negative):_constant(constant),_point(point),_polynom(polynom),_negative(negative){}
+Predicate::~Predicate(){}
+Polynom Predicate::get_polynom()
+{
+	return _polynom;
+}
+double Predicate::get_point()
+{
+	return _point;
+}
+double Predicate::get_constant()
+{
+	return _constant;
+}
+double Predicate::polynom_in_point()
+{
+	return this->get_polynom().get_value(this->get_point());
+}
+
+bool Predicate::get_negative()
+{
+	return _negative;
+}
+//bool Predicate::calculate()
+//{
+//	return true;
+//}
+Equality_predicate::Equality_predicate(double constant, double point, Polynom polynom,bool negative) : Predicate(constant,point,polynom,negative){}
+Equality_predicate::~Equality_predicate(){}
+
+bool Equality_predicate::calculate()
+{
+	bool negative=this->get_negative();
+	if (this->get_constant()==this->polynom_in_point())
+		return true*(!negative);
+	return !(true*(!negative));
+}
+
+Lesser_predicate::Lesser_predicate(double constant, double point, Polynom polynom, bool negative) : Predicate(constant,point,polynom,negative){}
+Lesser_predicate::~Lesser_predicate(){}
+
+bool Lesser_predicate::calculate()
+{
+	bool negative=this->get_negative();
+	//if polynom leq then constant
+	if (this->polynom_in_point()>=this->get_constant())
+		return true*(!negative);
+	return !(false*(!negative));
+}
+
+Conjunct::Conjunct(std::vector<Predicate*> predicats): _predicats(predicats) {}
+
+bool Conjunct::calculate()
+{
+	for (auto predicate : _predicats)
+	{
+//		std::cout<<predicate->calculate()<<std::endl;
+		if (predicate->calculate()==false)
+			return false;
+	}
+	return true;
+}
+
+void Conjunct::add_predicate(Predicate * predicate)
+{
+	_predicats.push_back(predicate);
+}
+
+DNF::DNF(std::vector<Conjunct> conjuncts) : _conjuncts(conjuncts){}
+bool DNF::calculate()
+{
+	bool result = false;
+	for (auto  predicate: _conjuncts)
+		if (predicate.calculate()==true)
+			return true;
+	return false;
+}
+void DNF::add_conjunct(Conjunct conjunct)
+{
+	_conjuncts.push_back(conjunct);
+}
+
+
+
--- tests.cpp
+++ tests.cpp
@@ -1,11 +1,76 @@
 #include<iostream>
 #include<vector>
 #include"Formulas.hpp"
+#include<tuple>
+void polynoms_print();
+void degree_print();
+void divider_print();
+void predicate_print();
+
 int main()
 {
-	std::vector<double> E{1,1,1,1,1,1};
-	Polynom P(E);
-//	std::cout<<P.get_value(2);
-	P.get_derivative();
+//	polynoms_print();
+//	degree_print();
+	predicate_print();
 	return 0;
+
+}
+void predicate_print()
+{
+	double a = 1;
+	double b = 10;
+	double c = 1;
+	std::vector<Predicate*> predicats;
+	Lesser_predicate pred1(0,1,Polynom(std::vector<double>{-4*a*c,0.0,b*b}),0);
+	predicats.push_back(&pred1);
+	Equality_predicate pred2(0,1,Polynom(std::vector<double>{a}),1);
+	predicats.push_back(&pred2);
+	Conjunct two_roots(predicats);
+	std::cout<<two_roots.calculate()<<std::endl;
+}
+void polynoms_print()
+{
+	std::vector<double> a{1,-8,5,-1,2};
+	std::vector<double> b{1,-1,1};
+	std::vector<double> c{4,1,2};
+	Polynom A(a);
+	Polynom B(b);
+	Polynom C(c);
+	std::cout<<"Polynom creation checking..."<<std::endl;
+	std::cout<<"Polynom A is : "<<std::endl;
+	A.print();
+	std::cout<<"Polynom B is : "<<std::endl;
+	B.print();
+	std::cout<<"Polynom C is : "<<std::endl;
+	std::cout<<"Polynom creation ended..."<<std::endl;
+	C.print();	
+}
+
+void degree_print()
+{
+	std::vector<double> a{1,-8,5,-1,2};
+	std::vector<double> b{1,-1,1};
+	std::vector<double> c{4,1,2};
+	Polynom A(a);
+	Polynom B(b);
+	Polynom C(c);
+	std::cout<<"Polynom deg checking..."<<std::endl;
+	std::cout<<" deg(A) is "<<A.get_degree()<<std::endl;
+	std::cout<<" deg(B) is "<<B.get_degree()<<std::endl;
+	std::cout<<" deg(C) is "<<C.get_degree()<<std::endl;
+	std::cout<<"Polynom deg ended..."<<std::endl;
+}
+
+void divider_print()
+{
+	std::vector<double> a{2,-1,5,-8,1};
+	std::vector<double> b{1,-1,1};
+	Polynom A(a);
+	Polynom B(b);
+	std::cout<<"Polynom dividing checking..."<<std::endl;
+	std::cout<<"Divident is";
+	A.print();
+	std::cout<<"Divider is";
+	B.print();
+	std::cout<<"Polynom dividing ended"<<std::endl;
 }
